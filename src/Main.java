7f904574b3b4183388c36732de0827a50be31612
manasvinir <manasvini.raghunathan@gmail.com>
2025-07-04 23:36:01 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+
+
+
+    //adding value of 2 strings
+    public static String problem2() {
+        StringBuilder result = new StringBuilder();
+
+        return result.toString();
+    }
+
+    //adjusting paragraph formatting with width
+    public static List<String> problem3() {
+        List<String> result = new LinkedList<>();
+
+        return result;
+
+    }
+
+    //3 arrays operations
+    public static int[] problem4() {
+        int[] answer = new int[];
+
+        return answer;
+    }
+
+}
